745dbea9da9681944a0961d8504c8394dac0d7f3
Anthony Goddard <anthony@anthonygoddard.com>
2013-04-03 00:25:20 -0400

    key as argument

--- program-generator.rb
+++ program-generator.rb
@@ -2,7 +2,8 @@ require 'google_drive'
 
 account = YAML.load_file('./account.yaml')
 session = GoogleDrive.login(account['mail'], account['password'])
-ws = session.spreadsheet_by_key("0AoUCodwCv313dDROMHc5cXBaM3dzbDkxS0c3RjllSnc").worksheets[0]
+key = ARGV[0]
+ws = session.spreadsheet_by_key(key).worksheets[0]
 
 ws.rows.each_with_index do |row,index|
 
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 reads google doc spreadsheet and turns it into HTML for the devopsdays.org
+
+Pass the spreadsheet key as the first argument to the script
+
+`ruby program-generator.rb (spreadsheet_key)`
+
+Only tested so far with the format of the Austin spreadsheet, will add a public example here
